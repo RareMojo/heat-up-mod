1282136a5c55162bcb36d71a6d1fe14f039bcb23
RareMojo <nickrejcek@gmail.com>
2023-02-11 17:28:09 -0600

    first main push

---
+++ src/main/java/raremojo/heat/up/HeatUpClient.java
@@ -0,0 +1,11 @@
+package raremojo.heat.up;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HeatUpClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // This code runs on the client only.
+        // You can use this to add things like custom textures and models.
+    }
+}
